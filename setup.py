769d65c1cea0aa750c95ed906b10a04331e0ee0b
Hsiaoming Yang <me@lepture.com>
2018-09-16 11:31:55 +0900

    Version bump 0.2

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
