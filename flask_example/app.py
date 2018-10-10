4b9c103de27de7cdf2b74b056d792928feb139dc
Hsiaoming Yang <me@lepture.com>
2018-10-11 08:40:39 +0900

    OAuth1 requires a cache instance.
    ref: https://github.com/authlib/loginpass/issues/11

--- flask_example/app.py
+++ flask_example/app.py
@@ -15,7 +15,24 @@ OAUTH_BACKENDS = [
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-oauth = OAuth(app)
+
+
+class Cache(object):
+    def __init__(self):
+        self._data = {}
+
+    def get(self, k):
+        return self._data.get(k)
+
+    def set(self, k, v, timeout=None):
+        self._data[k] = v
+
+    def delete(self, k):
+        if k in self._data:
+            del self._data[k]
+
+
+oauth = OAuth(app, Cache())
 
 
 @app.route('/')
