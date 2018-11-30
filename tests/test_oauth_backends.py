8e078ec219f0d7d37235b1e8fb193c77fbcf3874
Hsiaoming Yang <me@lepture.com>
2018-11-30 15:13:13 +0900

    Use unittest

--- tests/test_oauth_backends.py
+++ tests/test_oauth_backends.py
@@ -1,6 +1,7 @@
 import os
 import json
 import mock
+import unittest
 import requests
 from loginpass._core import UserInfo
 from loginpass import (
@@ -13,51 +14,48 @@ from loginpass import (
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def run_profile(backend, **kwargs):
-    filename = '{}_response.json'.format(backend.OAUTH_NAME)
-    with open(os.path.join(TEST_DIR, 'data', filename), 'r') as f:
-        resp = mock.MagicMock(spec=requests.Response)
-        data = json.load(f)
-        resp.json = lambda: data
-        resp.status_code = 200
+class TestOAuthBackends(unittest.TestCase):
+    def run_profile(self, backend, **kwargs):
+        filename = '{}_response.json'.format(backend.OAUTH_NAME)
+        with open(os.path.join(TEST_DIR, 'data', filename), 'r') as f:
+            resp = mock.MagicMock(spec=requests.Response)
+            data = json.load(f)
+            resp.json = lambda: data
+            resp.status_code = 200
 
-    with mock.patch('requests.sessions.Session.send') as send:
-        send.return_value = resp
-        profile = backend.profile(**kwargs)
-        assert isinstance(profile, UserInfo)
+        with mock.patch('requests.sessions.Session.send') as send:
+            send.return_value = resp
+            profile = backend.profile(**kwargs)
+            self.assertIsInstance(profile, UserInfo)
 
-    filename = '{}_result.json'.format(backend.OAUTH_NAME)
-    with open(os.path.join(TEST_DIR, 'data', filename), 'r') as f:
-        result = json.load(f)
-        assert dict(profile) == result
+        filename = '{}_result.json'.format(backend.OAUTH_NAME)
+        with open(os.path.join(TEST_DIR, 'data', filename), 'r') as f:
+            result = json.load(f)
+            self.assertEqual(dict(profile), result)
 
+    def run_oauth_profile(self, backend_cls):
+        c = backend_cls(
+            client_id='a',
+            client_secret='b',
+            **backend_cls.OAUTH_CONFIG
+        )
+        if c.OAUTH_TYPE == '1.0':
+            token = {
+                'oauth_token': 'a',
+                'oauth_token_secret': 'b'
+            }
+        else:
+            token = {
+                'token_type': 'bearer',
+                'access_token': 'a'
+            }
+        self.run_profile(c, token=token)
 
-def run_oauth_profile(backend_cls):
-    c = backend_cls(
-        client_id='a',
-        client_secret='b',
-        **backend_cls.OAUTH_CONFIG
-    )
-    if c.OAUTH_TYPE == '1.0':
-        token = {
-            'oauth_token': 'a',
-            'oauth_token_secret': 'b'
-        }
-    else:
-        token = {
-            'token_type': 'bearer',
-            'access_token': 'a'
-        }
-    run_profile(c, token=token)
+    def test_twitter(self):
+        self.run_oauth_profile(Twitter)
 
+    def test_github(self):
+        self.run_oauth_profile(GitHub)
 
-def test_twitter():
-    run_oauth_profile(Twitter)
-
-
-def test_github():
-    run_oauth_profile(GitHub)
-
-
-def test_yandex():
-    run_oauth_profile(Yandex)
+    def test_yandex(self):
+        self.run_oauth_profile(Yandex)
