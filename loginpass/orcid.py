212c26b93e8bb4e66f4edb6a43927b72905e1e63
Bryan Newbold <bnewbold@archive.org>
2019-04-10 15:58:30 -0700

    orcid returned iss hostname doesn't include final slash

--- loginpass/orcid.py
+++ loginpass/orcid.py
@@ -16,20 +16,20 @@ st
 from ._core import UserInfo, OAuthBackend, parse_id_token
 
 
-_host = 'https://orcid.org/'
-_authorize_url = '{}oauth/authorize'.format(_host)
-_token_url = '{}oauth/token'.format(_host)
+_host = 'https://orcid.org'
+_authorize_url = '{}/oauth/authorize'.format(_host)
+_token_url = '{}/oauth/token'.format(_host)
 
 class ORCiD(OAuthBackend):
     OAUTH_TYPE = '2.0,oidc'
     OAUTH_NAME = 'orcid'
     OAUTH_CONFIG = {
-        'api_base_url': 'https://orcid.org/',
+        'api_base_url': _host,
         'access_token_url': _token_url,
         'authorize_url': _authorize_url,
         'client_kwargs': {'scope': 'openid email profile'},
     }
-    JWK_SET_URL = '{}oauth/jwks'.format(_host)
+    JWK_SET_URL = '{}/oauth/jwks'.format(_host)
 
     def profile(self, **kwargs):
         resp = self.get('userinfo', **kwargs)
