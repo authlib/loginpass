f07decbaa675691395f252a6cc234dc8a10dc5d7
Tomas Kulhanek <tkulhanek@esciencefactory.com>
2019-07-29 07:27:54 +0100

    UPDATE: passing base_url in function create_b2access_backend, default B2Access and B2AccessDev instances are provided

--- loginpass/b2access.py
+++ loginpass/b2access.py
@@ -6,7 +6,7 @@
     gets endpoint from B2ACCESS_API_URL environment variable, if not specified, default development url is used
 
     development: https://unity.eudat-aai.fz-juelich.de/
-    production: https://b2access.eudat.eu/
+    production: https://b2access.eudat.eu).
 
     :copyright: (c) 2019 by Tomas Kulhanek, ESDF
     :license: MIT, see LICENSE for more details.
@@ -22,31 +22,34 @@ import os
 #User information 	https://unity.eudat-aai.fz-juelich.de:443/oauth2/userinfo 	https://b2access.eudat.eu:443/oauth2/userinfo
 
 # development endpoint
-B2ACCESS_API_URL = os.environ.get('B2ACCESS_API_URL',default='https://unity.eudat-aai.fz-juelich.de/')
-B2ACCESS_TOKEN_URL = B2ACCESS_API_URL + 'oauth2/token'
-B2ACCESS_TOKENINFO_SUFFIX='oauth2/tokeninfo'
-B2ACCESS_TOKENINFO_URL = B2ACCESS_API_URL + B2ACCESS_TOKENINFO_SUFFIX
-B2ACCESS_AUTH_URL = B2ACCESS_API_URL + 'oauth2-as/oauth2-authz'
-B2ACCESS_USERINFO_SUFFIX='oauth2/userinfo'
-B2ACCESS_USERINFO_URL = B2ACCESS_API_URL + B2ACCESS_USERINFO_SUFFIX
 
-class B2Access(OAuthBackend):
-    OAUTH_TYPE = '2.0,oidc'
-    OAUTH_NAME = 'b2access'
-    OAUTH_CONFIG = {
-        'api_base_url': B2ACCESS_API_URL,
-        'access_token_url': B2ACCESS_TOKEN_URL,
-        'authorize_url': B2ACCESS_AUTH_URL,
-        'client_kwargs': {'scope': 'email profile'},
-    }
-
-    def profile(self, **kwargs):
-        print('b2access profile kwargs:',kwargs)
-        resp = self.get(B2ACCESS_USERINFO_SUFFIX,**kwargs)
-        data = resp.json()
-        params = {
-            'sub': data['sub'],
-            'name': data['name'],
-            'email': data['email'],
+def create_b2access_backend(name,b2accessurl):
+    B2ACCESS_API_URL = b2accessurl.strip('/')
+    B2ACCESS_TOKEN_URL = '{b2accessurl}/oauth2/token'.format(b2accessurl=b2accessurl)
+    B2ACCESS_TOKENINFO_URL = '{b2accessurl}/oauth2/tokeninfo'.format(b2accessurl=b2accessurl)
+    B2ACCESS_AUTH_URL = '{b2accessurl}/oauth2-as/oauth2-authz'.format(b2accessurl=b2accessurl)
+    B2ACCESS_USERINFO_SUFFIX='/oauth2/userinfo'
+    B2ACCESS_USERINFO_URL = '{b2accessurl}/oauth2/userinfo'.format(b2accessurl=b2accessurl)
+    class B2Access(OAuthBackend):
+        OAUTH_TYPE = '2.0,oidc'
+        OAUTH_NAME = 'b2access'
+        OAUTH_CONFIG = {
+            'api_base_url': B2ACCESS_API_URL,
+            'access_token_url': B2ACCESS_TOKEN_URL,
+            'authorize_url': B2ACCESS_AUTH_URL,
+            'client_kwargs': {'scope': 'email profile'},
         }
-        return UserInfo(params)
+
+        def profile(self, **kwargs):
+            print('b2access profile kwargs:',kwargs)
+            resp = self.get(B2ACCESS_USERINFO_SUFFIX,**kwargs)
+            data = resp.json()
+            params = {
+                'sub': data['sub'],
+                'name': data['name'],
+                'email': data['email'],
+            }
+            return UserInfo(params)
+
+B2Access = create_b2access_backend('b2access','https://b2access.eudat.eu')
+B2AccessDev = create_b2access_backend('b2access','https://unity.eudat-aai.fz-juelich.de')
