8507bebe387adf14201b416286bda496605616ba
Matthias De Smet <matthias.desmet@ugent.be>
2019-10-09 09:36:08 +0200

    WIP: add hydra support

--- django_example/website/settings.py
+++ django_example/website/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '-ldq4(wl8r9#-($xa7_$2x*ghdvmu#n7@geocf*z#%2l*^0r5i'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,7 +67,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'website.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
@@ -79,7 +76,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -99,7 +95,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -112,7 +107,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
@@ -153,8 +147,12 @@ AUTHLIB_OAUTH_CLIENTS = {
         'client_id': '',
         'client_secret': '',
         'client_kwargs': {
-            'api_key': '',     # required
+            'api_key': '',  # required
             'api_filter': '',  # optional
         }
+    },
+    'hydra': {
+        'client_id': '',
+        'client_secret': '',
     }
 }
